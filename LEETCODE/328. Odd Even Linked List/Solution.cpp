0e11e170483f86291434c9d2432144e663f31751
Devam <devamdp27@gmail.com>
2025-09-04 18:04:10 +0530

    LEETCODE 75

---
+++ LEETCODE/328. Odd Even Linked List/Solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL || head->next->next == NULL){
+            return head;
+        }
+        ListNode* temp = head->next;
+        ListNode* curr = temp;
+        ListNode* prev = head;
+        while(curr != NULL && curr->next != NULL){
+            prev->next = curr->next;
+            prev = prev->next;
+            curr->next = prev->next;
+            curr = curr->next;
+        }
+        prev->next = temp;
+        return head;
+    }
+};
