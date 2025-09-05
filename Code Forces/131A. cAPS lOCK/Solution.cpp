9399cea5686f31b6db7f71d3a19b182a40e490ab
Devam <devamdp27@gmail.com>
2025-09-05 13:58:08 +0530

    Day 43 Beginners Track

---
+++ Code Forces/131A. cAPS lOCK/Solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+ 
+    bool check = true;
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (islower(s[i]))
+        {
+            check = false;
+            break;
+        }
+    }
+ 
+    if (check)
+    {
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (islower(s[i]))
+            {
+                s[i] = toupper(s[i]);
+            }
+            else
+            {
+                s[i] = tolower(s[i]);
+            }
+        }
+    }
+ 
+    cout << s << endl;
+}
