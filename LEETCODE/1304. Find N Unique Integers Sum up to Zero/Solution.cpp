ff9c27aab859ba606a1853c4e9539d85b2cf330e
Devam <devamdp27@gmail.com>
2025-09-07 23:19:07 +0530

    POTD 39

---
+++ LEETCODE/1304. Find N Unique Integers Sum up to Zero/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        if(n%2==0){
+            for(int i = 0-n/2; i <= n/2; i++){
+                if(i == 0){
+                    continue;
+                }
+                ans.push_back(i);
+            }
+        }
+        else{
+            for(int i = 0-n/2; i <= n/2; i++){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
