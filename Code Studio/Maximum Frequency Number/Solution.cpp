91a54ef0709356cb39e309966765017b66be90a3
Devam <devamdp27@gmail.com>
2026-09-02 02:32:10 +0530

    02/09/26

---
+++ Code Studio/Maximum Frequency Number/Solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int maximumFrequency(vector<int> &arr, int n)
+{
+    unordered_map<int, int> m;
+    int maxFreq = 0;
+    int maxAns = 0;
+    for (int i = 0; i < n; i++)
+    {
+        m[arr[i]]++;
+        maxFreq = max(maxFreq, m[arr[i]]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (maxFreq == m[arr[i]])
+        {
+            maxAns = arr[i];
+            break;
+        }
+    }
+
+    return maxAns;
+}
